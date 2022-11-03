00aafdd00c3f799052cc37577b6d49008916ea8d
Anthony Tong <atong28.usa@gmail.com>
2022-11-03 12:14:01 -0700

    Create battleship.py

---
+++ battleship.py
@@ -0,0 +1,23 @@
+import argparse
+import time
+import os
+
+class Battleship():
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Battleship Tester')
+    # args = parser.parse_args()
+
+    '''arguments = [args.username, args.password, args.ip]
+    if (args.debug):
+        try:
+            arguments.append(bool(args.debug))
+        except Exception:
+            print("Potentially invalid debug argument. Please enter True or False")
+    '''
+
+    run = Battleship()
