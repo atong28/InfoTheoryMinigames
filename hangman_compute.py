0f1111f4f73c035f1b56894142347a6eeedb9dc6
Anthony Tong <atong28.usa@gmail.com>
2022-11-26 23:18:51 -0800

    Update hangman_compute.py

--- hangman_compute.py
+++ hangman_compute.py
@@ -2,15 +2,20 @@ import numpy as np
 from string import ascii_lowercase as alc
 import fnmatch
 
-f = open('top50k.txt', 'r', encoding='utf-8')
+f = open('wikipedia-english-list.txt', 'r', encoding='utf-8')
 content = f.read()
 
 def splitText(entry):
     result = entry.split(" ")
     return (result[0], int(result[1]))
 
-def matchesFilter(str, filter):
+def matchesFilter(str, filter, used_letters):
     if len(str) != len(filter): return False
+    
+    for letter in used_letters:
+        if letter in filter: continue
+        if letter in str: return False
+    
     for i in range(len(str)):
         if filter[i] == "?": continue
         if filter[i] != str[i]: return False
@@ -18,18 +23,23 @@ def matchesFilter(str, filter):
 
 wordList = list(map(splitText, content.splitlines()))
 
-sortedList = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+sortedList = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
 
 for word in wordList:
     if not word[0].isascii(): continue
-    if len(word[0]) > 39: print(word)
+    if len(word[0]) > 39: continue
     sortedList[len(word[0])][word[0]] = word[1]
 
-def findBestLetter(filter, wordlist):
-    newFilteredList = {k:v for k,v in wordlist.items() if matchesFilter(k, filter)}
+print("Loaded the wordlist.")
+
+def findBestLetter(filter, wordlist, used_letters):
+    
+    print(f"Finding best letter with filter {filter}, used letters {used_letters}")
+    
+    newFilteredList = {k:v for k,v in wordlist.items() if matchesFilter(k, filter, used_letters)}
 
     # intialize the dictionary of frequencies
-    alphabetList = {'-':0, "'":0}
+    alphabetList = {'-':0, "'":0, "_":0}
     for letter in alc:
         alphabetList[letter] = 0
     
@@ -42,6 +52,12 @@ def findBestLetter(filter, wordlist):
     total = sum(alphabetList.values())
     frequencies = {k: v * 4 / total for k, v in alphabetList.items()}
 
+    # remove letters already used
+    for letter in used_letters:
+        frequencies[letter] = 0
+
+    print(frequencies)
+
     normalized = [abs(v - 0.5) for v in frequencies.values()]
 
     i = normalized.index(min(normalized))
@@ -52,5 +68,9 @@ def findBestLetter(filter, wordlist):
 
 while True:
     filter = input("Input the filter: ")
-    length = int(input("Input the length of the word: "))
-    findBestLetter(filter, sortedList[length])
+    used_letters = input("Input the used letters: ")
+    
+    if used_letters == '':
+        findBestLetter(filter, sortedList[len(filter)], [])
+    else:
+        findBestLetter(filter, sortedList[len(filter)], [*used_letters])
