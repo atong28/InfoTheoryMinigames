52933a7d9839ecc5af62514138facc0f64ba7308
Anthony Tong <atong28.usa@gmail.com>
2022-11-28 11:49:30 -0800

    organize battleship

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -1,19 +1,6 @@
 import numpy as np
 from numpy import unravel_index
-
-################################################################################
-# Text color presets.                                                          #
-################################################################################
-class bcolors:
-    RED = '\u001b[31;1m'
-    BLUE = '\u001b[34;1m'
-    YELLOW = '\u001b[33;1m'
-    GREEN = '\u001b[32;1m'
-    MAGENTA = '\u001b[35;1m'
-    CYAN = '\u001b[36;1m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    RESET = '\u001b[0m'
+import lib.colors as bcolors
 
 ################################################################################
 # BATTLESHIP CLASS: Runs the game.                                             #
--- battleship/battleship_new.py
+++ battleship/battleship_new.py
@@ -1,20 +1,7 @@
 import numpy as np
 from numpy import unravel_index
 import copy
-
-################################################################################
-# Text color presets.                                                          #
-################################################################################
-class bcolors:
-    RED = '\u001b[31;1m'
-    BLUE = '\u001b[34;1m'
-    YELLOW = '\u001b[33;1m'
-    GREEN = '\u001b[32;1m'
-    MAGENTA = '\u001b[35;1m'
-    CYAN = '\u001b[36;1m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    RESET = '\u001b[0m'
+import lib.colors as bcolors
 
 def isHit(board):
     
