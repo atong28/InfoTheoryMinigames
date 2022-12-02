12acc26231b55c59f8fd9d1d8d8ddf3d4562a0f4
Anthony Tong <atong28.usa@gmail.com>
2022-12-02 11:58:51 -0800

    testing stuff

---
+++ test2.py
@@ -0,0 +1,16 @@
+import os
+f = open(os.path.abspath('bnc-pruned.lm'), 'r', encoding='utf-8')
+content = f.read().splitlines()
+
+index1 = content.index('\\1-grams:') + 1
+index2 = content.index('\\2-grams:') - 1
+
+unigrams = content[index1:index2]
+
+sumProb = 0
+
+for entry in unigrams:
+    split = entry.split()
+    sumProb += 10 ** eval(split[0]) 
+
+print(sumProb)
