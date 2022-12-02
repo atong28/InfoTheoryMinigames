27e1c2641a8cace8b899b1e6aaab3ac89e0581b7
Anthony Tong <atong28.usa@gmail.com>
2022-12-02 14:38:54 -0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,8 +2,8 @@ import os
 f = open(os.path.abspath('bnc-pruned.lm'), 'r', encoding='utf-8')
 content = f.read().splitlines()
 
-index1 = content.index('\\1-grams:') + 1
-index2 = content.index('\\2-grams:') - 1
+index1 = content.index('\\2-grams:') + 1
+index2 = content.index('\\3-grams:') - 1
 
 unigrams = content[index1:index2]
 
